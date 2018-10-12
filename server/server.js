59a505573a257fbc8a47a9fb796d5c648c98c971
Akshay Tenkale <tenkaleakshay94@gmail.com>
2018-10-12 14:41:31 -0400

    Get todos by ID

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+
 
 var app = express();
 
@@ -28,6 +30,24 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+
+app.get('/todos/:id' ,(req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send(e);
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      res.status(400).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
