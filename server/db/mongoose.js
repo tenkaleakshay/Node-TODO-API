e883657934f7b6afa6b3d79547181e02a0f21f75
Akshay Tenkale <tenkaleakshay94@gmail.com>
2018-10-19 17:13:35 -0400

    Added Config json

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
-module.exports ={mongoose};
+module.exports = {mongoose};
